e215b0c6472f01f77ab9e1887d2ac8a0c277cc85
mkandil <mohanadmkandil@gmail.com>
2026-01-01 00:47:49 +0100

    add learned tokens in human readable way

--- bpe/src/main.rs
+++ bpe/src/main.rs
@@ -5,7 +5,8 @@ use std::vec::Vec;
 fn main() -> std::io::Result<()> {
     let data = read("src/input.txt")?;
     let mut ids: Vec<u32> = data.into_iter().map(|b| b as u32).collect();
-    let mut merges: HashMap<u32, (u32, u32)> = HashMap::new();
+    let mut merges: HashMap<(u32, u32), u32> = HashMap::new();
+    let mut vocab: HashMap<u32, Vec<u8>> = HashMap::new();
 
     // Create a counter for new IDs
     let mut next_id = 256;
@@ -31,7 +32,7 @@ fn main() -> std::io::Result<()> {
             if i < ids.len() - 1 && ids[i] == most_common_pair.0 && ids[i + 1] == most_common_pair.1
             {
                 new_ids.push(next_id); // Use our counter!
-                merges.insert(next_id, *most_common_pair);
+                merges.insert(*most_common_pair, next_id);
                 i += 2;
             } else {
                 new_ids.push(ids[i]);
@@ -51,6 +52,28 @@ fn main() -> std::io::Result<()> {
         // Update for next round
         ids = new_ids;
         next_id += 1;
+
+        for i in 0..256 {
+            vocab.insert(i as u32, vec![i as u8]);
+        }
+        // build vocab
+        let mut sorted_merges: Vec<_> = merges.iter().collect();
+        sorted_merges.sort_by_key(|(key, _)| key.0);
+
+        for ((p1, p2), new_id) in sorted_merges {
+            let mut combined = vocab.get(p1).unwrap().clone();
+            combined.extend(vocab.get(p2).unwrap());
+            vocab.insert(*new_id, combined);
+        }
+
+        // print the result
+        println!("--- TOP LEARNED TOKENS ---");
+        for i in 256..next_id {
+            if let Some(bytes) = vocab.get(&i) {
+                let s = String::from_utf8_lossy(bytes);
+                println!("ID {}: {:?}", i, s);
+            }
+        }
     }
 
     Ok(())
